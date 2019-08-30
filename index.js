f53510fbd6ca55211fc464807447ad9ae3cd5719
CamilaRivera <camila.riverart@gmail.com>
2019-08-30 00:27:31 +0000

    nextISSTiesForMyLocation done and refactor with lint

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP("162.245.144.188", (error , coords) => {
-//   if (error) {
-//     console.log('Error', error);
-//   } else {
-//     console.log('It worked! Returned Coords:' ,coords);
-//   }
-// });
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  for (let dateTime of passTimes) {
+    console.log(`Next pass at ${new Date(dateTime.risetime)} for ${dateTime.duration} seconds`);
+  }
+});
